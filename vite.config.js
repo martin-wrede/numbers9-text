f5c4c0bd804b0db08fe1168d541945c9be97641a
martin-wrede <81897938+martin-wrede@users.noreply.github.com>
2023-03-10 11:54:01 +0100

    3rd commit, codes fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/numbers8-text/',
+  base:'/numbers9-text/',
   plugins: [react()]
 })
